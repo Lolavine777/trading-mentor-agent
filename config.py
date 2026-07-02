98e1ba73dc76c36fe74d224f7013fa853e032d4f
Lolavine777 <thudomade@gmail.com>
2026-07-02 14:42:26 +0700

    fix: thay đổi tên model cho phù hợp với phiên bản gemini

--- config.py
+++ config.py
@@ -6,5 +6,5 @@ load_dotenv()
 GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "")
 TELEGRAM_TOKEN = os.environ.get("TELEGRAM_TOKEN", "")
 TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID", "")
-model_name = "gemini-3.5-flash-lite"
+model_name = "gemini-3.5-flash"
 temperature = 0.7
